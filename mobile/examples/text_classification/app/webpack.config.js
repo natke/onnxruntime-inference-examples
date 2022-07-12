5830b290c5ee6652c10d02af3471976b56f4a6d6
Nat Kershaw <nakersha@microsoft.com>
2022-07-12 15:26:59 -0700

    Add manual step to copy wasm from onnxruntime-web

--- mobile/examples/text_classification/app/webpack.config.js
+++ mobile/examples/text_classification/app/webpack.config.js
@@ -1,7 +1,26 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = async function (env, argv) {
+
   const config = await createExpoWebpackConfigAsync(env, argv);
-  // Customize the config before returning it.
+
+  config.plugins.push(
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './node_modules/onnxruntime-web/dist/ort-wasm.wasm',
+          to: 'static/chunks/pages',
+        },             {
+          from: './node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm',
+          to: 'static/chunks/pages',
+        }]          
+      }
+    )
+  )
+
   return config;
 };
+
+
